0aeeae495b160d9f813190d693e5d7b9edb4af6d
Amankumar279 <amankumar279@gmail.com>
2025-02-04 20:41:59 +0530

    First-commit

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const  connectDB = async () =>{
+
+    await mongoose.connect('mongodb+srv://iamankumar279:2709ak47@cluster0.zhiem.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+   
+}
+
+
