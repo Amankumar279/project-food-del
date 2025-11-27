3387750e37c835df95ff4a478430e38fb3689d91
Aman Kumar <iamankumar279@gmail.com>
2025-11-27 15:18:03 +0530

    added Cloudinary

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://iamankumar279:2709ak47@cluster0.zhiem.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose . connect ('mongodb+srv://iamankumar279:2709ak47@cluster0.zhiem.mongodb.net/food-del').then ( ( )=>console. log( "DB Connected" ));
    
 }
 
